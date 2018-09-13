b383a40dab1fcd75192b110ffcd7b483d9de02e4
Kasza <chris.kasza@gmail.com>
2018-09-12 22:58:04 -0300

    Added prompt loop

--- a01/src/main.c
+++ a01/src/main.c
@@ -5,9 +5,11 @@
  */
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 extern int errno;
 
@@ -16,7 +18,7 @@ extern int errno;
  */
 void print_help()
 {
-    print('Usage: kasza_a01 [DIR]\n');
+    printf("Usage: kasza_a01 [DIR]\n");
 }
 
 /**
@@ -24,9 +26,10 @@ void print_help()
  * files are stored.
  * If the directory doesn't exist, it will be created.
  * If the path is not a directory or if the directory
- * is inaccessible, the method will kill the program.
+ * is inaccessible, the method will exit with a status of 0.
+ * Otherwise, a success will return 1.
  */
-void init_dir(const char *path)
+int init_dir(const char *path)
 {
     int e;
     struct stat path_stat;
@@ -35,13 +38,13 @@ void init_dir(const char *path)
     {
         if (S_ISDIR(path_stat.st_mode))
         {
-            return;
+            return 1;
         }
         else
         {
-            printf("%s is not directory.\n", path);
+            printf("%s is not a directory.\n", path);
             print_help();
-            EXIT_FAILURE;
+            return 0;
         }
     }
     else
@@ -52,17 +55,17 @@ void init_dir(const char *path)
             if (e != 0)
             {
                 printf("unable to create %s; errno=%d\n", path, errno);
-                EXIT_FAILURE;
+                return 0;
             }
             else
             {
-                return;
+                return 1;
             }
         }
         else
         {
             printf("unable to read %s; errno=%d\n", path, errno);
-            EXIT_FAILURE;
+            return 0;
         }
     }
 }
@@ -72,9 +75,61 @@ void init_dir(const char *path)
  * Parses the user's input.
  * Hands off user's request to proper function.
  */
-void prompt_loop() {
-    printf('> ');
+void prompt_loop()
+{
+    char command[255], cmd_type[25];
+    // (char*)malloc(255 * sizeof(char));
 
+    while (1)
+    {
+        printf("> ");
+
+        // scanf("%s", command);
+        fgets(command, 255, stdin);
+
+        if ((strlen(command) > 0) && (command[strlen(command) - 1] == '\n'))
+            command[strlen(command) - 1] = '\0';
+
+        for (int i = 0; command[i]; i++)
+        {
+            if (command[i] == ' ')
+                break;
+            cmd_type[i] = tolower(command[i]);
+        }
+
+        if (strcmp(cmd_type, "add") == 0)
+        {
+            printf("gonna add things\n");
+            // add(command);
+        }
+        else if (strcmp(cmd_type, "show") == 0)
+        {
+            printf("gonna show things\n");
+            // show(command);
+        }
+        else if (strcmp(cmd_type, "delete") == 0)
+        {
+            printf("gonna delete things\n");
+            // delete(command);
+        }
+        else if (strcmp(cmd_type, "set") == 0)
+        {
+            printf("gonna set things\n");
+            // set(command);
+        }
+        else if (strcmp(cmd_type, "quit") == 0)
+        {
+            return;
+        }
+        else
+        {
+            printf("\"%s\" is not a recognized command.\n", cmd_type);
+            printf("Valid commands are ADD, SHOW, DELETE, SET, and QUIT.\n");
+        }
+
+        memset(cmd_type, 0, sizeof(cmd_type));
+        memset(command, 0, sizeof(command));
+    }
 }
 
 /**
@@ -83,20 +138,25 @@ void prompt_loop() {
 int main(int argc, char *argv[])
 {
     int e;
-    char *dir; // directory where files are stored
+    char dir[255]; // directory where files are stored
 
     // set dir if provided
     // otherwise, use /tmp/kasza_a1
     if (argc > 1)
     {
-        dir = argv[1];
+        strcpy(dir, argv[1]);
     }
     else
     {
-        dir = '/tmp/kasza_a1';
+        strcpy(dir, "/tmp/kasza_a1");
     }
 
-    init_dir(dir);
+    if (init_dir(dir) == 0)
+    {
+        return EXIT_FAILURE;
+    }
 
-    EXIT_SUCCESS;
+    prompt_loop();
+
+    return EXIT_SUCCESS;
 }
